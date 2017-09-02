8c09d4e620836e88f0c999b742ff3d07ec589c49
Brelon Buckner <brelonbuckner@Brelons-MacBook-Air.local>
2017-09-02 10:00:58 -0400

    WIP: close but not close enough

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+//need a if statement for when wins = random number, increase (++) wins
+//need a if statement for when the players score > random number, increase (++) loses
+//Start up document.ready()
+// Global variables that I will be working with throughout the entirety of my logic
+//function that will give me a random number, 2 parameters are the min (19) and max (120)
+//One to reset game, one to check the score, and one to start the actual game, call at the bottom.
